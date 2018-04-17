eeccae0794b1ad1f093c997259bcdcd45fe8f176
Bas Jacobs <basjacobs93@gmail.com>
2018-04-17 23:52:46 +0200

    get actual plotting to work

--- plotter.py
+++ plotter.py
@@ -1,8 +1,8 @@
 def move(x, y):
-	return "G01 X{} Y{}".format(-x, y)
+	return "G01 X{} Y{}".format(round(-x, 5), round(y, 5))
     
 def move_quick(x, y):
-    return "G00 X{} Y{}".format(-x, y)
+    return "G00 X{} Y{}".format(round(-x, 5), round(y, 5))
     
 def set_speed(speed):
 	return "F{}".format(speed)
@@ -18,7 +18,7 @@ def set_unit():
     
 def arc(x, y, i, j, cw = True):
     if cw:
-        c = "3"
-    else:
         c = "2"
-    return "G0{} X{} Y{} I{} J{}".format(c, -x, y, -i, j)
+    else:
+        c = "3"
+    return "G0{} X{} Y{} I{} J{}".format(c, round(-x, 5), round(y, 5), round(-i, 5), round(j, 5))
